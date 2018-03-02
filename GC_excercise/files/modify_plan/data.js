d14b9f311bdbaeff5f3d0964b92e9a1415e78a8d
Hirak Choudhury <choudhuryhirakkumar@gmail.com>
2018-03-03 00:56:48 +0530

    GC_ex

---
+++ GC_excercise/files/modify_plan/data.js
@@ -0,0 +1,36 @@
+﻿$axure.loadCurrentPage({
+  "url":"modify_plan.html",
+  "generationDate":new Date(1520018789475.46),
+  "isCanvasEnabled":false,
+  "variables":["OnLoadVariable"],
+  "page":{
+    "packageId":"dd754cfd0e3e4f259e1c76a8f733984b",
+    "type":"Axure:Page",
+    "name":"Modify Plan",
+    "notes":{
+},
+    "style":{
+      "baseStyle":"627587b6038d43cca051c114ac41ad32",
+      "pageAlignment":"near",
+      "fill":{
+        "fillType":"solid",
+        "color":0xFFFFFFFF},
+      "image":null,
+      "imageHorizontalAlignment":"near",
+      "imageVerticalAlignment":"near",
+      "imageRepeat":"auto",
+      "favicon":null,
+      "sketchFactor":"0",
+      "colorStyle":"appliedColor",
+      "fontName":"Applied Font",
+      "borderWidth":"0"},
+    "adaptiveStyles":{
+},
+    "interactionMap":{
+},
+    "diagram":{
+      "objects":[]}},
+  "masters":{
+},
+  "objectPaths":{
+}});
