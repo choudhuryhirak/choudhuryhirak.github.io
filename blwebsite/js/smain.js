bc78b5fb3e5cae0d1aaf94320b9d914b335a3f3e
Hirak Choudhury <choudhuryhirakkumar@gmail.com>
2018-04-17 17:38:24 +0530

    deeporwidecommit

--- blwebsite/js/smain.js
+++ blwebsite/js/smain.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
     //})
     
     
-    var controller = new ScrollMagic.Controller();
+    var controller1 = new ScrollMagic.Controller();
     
     
     var animatein = new TimelineMax();
@@ -40,19 +40,38 @@ $(document).ready(function(){
     .from('#tsstyle', 0.6, {autoAlpha:0, ease:Power2.easeIn},0.4)
     .from('#taoofsixtitle', 0.5, {autoAlpha:0, ease:Power3.easeNone},0.6)
     .from('#bulb img', 2, {scaleX:0,scaleY:0},0.8)
-    .from('#taoofsix1', 2, {scaleX:0,scaleY:0},0.8)
-    .from('#taoofsix2', 2, {scaleX:0,scaleY:0},0.8);
+    .from('#taoofsix1', 0.6, {autoAlpha:0, x:"-100%",y:"0"},1.4)
+    .from('#taoofsix2', 0.6, {autoAlpha:0, x:"-50%",y:"0"},1.6);
     
     
     var bulbScene = new ScrollMagic.Scene({
         triggerElement: '#bulb',
-        triggerHook: 0.8,
+        triggerHook: 0.8
         
         
     })
     .setTween(animatein)
-    .addTo(controller);
+    .addTo(controller1);
     
+    
+    
+    var controller2 = new ScrollMagic.Controller();
+    
+    var animatein2 = new TimelineMax();
+    animatein2
+    .from('#sode img', 3, {autoAlpha:0, x:"50%", y:"0"},0.2)
+    .from('.dt', 0.6, {autoAlpha:0, x:"-25%", y:"0"},0.2)
+    .from('#styleimg', 0.8, {autoAlpha:0, ease:Power1.easeIn},0.4)
+    .from('#dwcopy', 0.6, {autoAlpha:0, x:"0", y:"15%"}, 0.6)
+    .from('#dwicon', 1.2, {autoAlpha:0, x:"0", y:"40%"}, 1.2)
+    ;
+    
+    var whyusScene = new ScrollMagic.Scene({
+        triggerElement: '#whyus',
+        triggerHook: 0.6
+    })
+    .setTween(animatein2)
+    .addTo(controller1);
     
     //build a scene
     
