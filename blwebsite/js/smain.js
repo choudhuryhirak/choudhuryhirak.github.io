9178cb914e37fdf05a128e79c72ad9ea842e3818
Hirak Choudhury <choudhuryhirakkumar@gmail.com>
2018-04-17 12:58:36 +0530

    int

--- blwebsite/js/smain.js
+++ blwebsite/js/smain.js
@@ -36,26 +36,20 @@ $(document).ready(function(){
     
     var animatein = new TimelineMax();
     animatein
-    .from('#bulb img', 0.6, {autoAlpha: 0, ease:Power0.easeNone}, 0.4);
+    .from('#dotitle', 0.8, {autoAlpha:0, ease:Power1.easeIn},0.8)
+    .from('#tsstyle', 0.6, {autoAlpha:0, ease:Power2.easeIn},0.6)
+    .from('#taoofsixtitle', 0.5, {autoAlpha:0, ease:Power3.easeNone},0.5)
+    .from('#bulb img', 0.4, {autoAlpha: 0, ease:Power4.easeNone}, 0.4);
     
     
     var bulbScene = new ScrollMagic.Scene({
-        triggerElement: '#bulb img',
-        triggerHook: 0.5,
-        duration: '20%'
+        triggerElement: '#bulb',
+        triggerHook: 0.8,
+        duration: '30%'
         
     })
     .setTween(animatein)
     .addTo(controller);
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     
     //build a scene
