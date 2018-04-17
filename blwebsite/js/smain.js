540bc172120347baafababb0b6eba9a2bc57c487
Hirak Choudhury <choudhuryhirakkumar@gmail.com>
2018-04-17 12:35:54 +0530

    Scrollmagic GSAP combo

--- blwebsite/js/smain.js
+++ blwebsite/js/smain.js
@@ -2,35 +2,65 @@ $(document).ready(function(){
     
     //Init Scrollmagic
     
+   // var controller = new ScrollMagic.Controller();
+    
+    //$('.container').each(function(){
+        
+        //var mainScene = new ScrollMagic.Scene({
+            //triggerElement: this.children[0],
+            //triggerHook: 0.8
+            
+            
+        //})
+        //.setClassToggle(this, 'fade-in')
+        //.addIndicators(
+            //{
+                //name: 'fade scene',
+                //colorTrigger: 'black',
+                //colorStart: 'orange',
+                //colo00000rEnd: 'indigo'
+
+            //})
+        
+        
+        
+        
+        //.addTo(controller);
+        
+        
+    //})
+    
+    
     var controller = new ScrollMagic.Controller();
     
-    $('.angle-bg sky-bg section-padding').each(function(){
-        
-        var mainScene = new ScrollMagic.Scene({
-            triggerElement: this.children[0],
-            triggerHook: 0.8
-            
-            
-        })
-        .setClassToggle(this, 'fade-in')
-        .addIndicators(
-            {
-                name: 'fade scene',
-                colorTrigger: 'black',
-                colorStart: 'orange',
-                colo00000rEnd: 'indigo'
-
-            })
-        
-        
-        
-        
-        .addTo(controller);
-        
+    
+    var animatein = new TimelineMax();
+    animatein
+    .from('#bulb img', 0.6, {autoAlpha: 0, ease:Power0.easeNone}, 0.4);
+    
+    
+    var bulbScene = new ScrollMagic.Scene({
+        triggerElement: '#bulb img',
+        triggerHook: 0.5,
+        duration: '20%'
         
     })
+    .setTween(animatein)
+    .addTo(controller);
     
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //build a scene
+    
+     
     
     
 });
